da0616ee468e1b6904e908b5c66925f7682f2bff
joe <malamas.alexis@gmail.com>
2017-03-07 15:02:22 +0100

    test

---
+++ src/main/Client.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+	public static void main(String args[])
+	{
+		try
+		{
+			System.out.println("CLIENT");
+			
+			int port = Integer.parseInt(args[0]);
+			Socket sock = new Socket(InetAddress.getLocalHost(),port);
+			
+			BufferedReader inchan = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			PrintStream canalEcriture= new PrintStream(sock.getOutputStream());
+			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+			
+			String ligne = new String();
+			
+			while(true)
+			{
+				System.out.println("?");
+				ligne = console.readLine();
+				
+
+				if(ligne == ""){System.out.println("Connexion terminee");sock.close();break;}
+				System.out.println("envoie de "+ligne);
+				canalEcriture.println(ligne);
+				canalEcriture.flush();
+				ligne = inchan.readLine();
+
+				System.out.println("!"+ligne);
+				
+				
+			}
+			
+		}
+		catch(Throwable t) {t.printStackTrace(System.err); }
+	}
+}
---
+++ src/main/EchoServer.java
@@ -0,0 +1,66 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+
+	public static void main(String args[])
+	{
+		BufferedReader inchan;
+		PrintWriter outchan;
+		ServerSocket serv;
+		Socket client;
+
+		try
+		{
+			
+			System.out.println("SERVEUR");
+			
+			int port = Integer.parseInt(args[0]);
+			System.out.println("port:"+port);
+			serv = new ServerSocket(port);
+			String command = "";
+
+			while(true)
+			{
+				client = serv.accept();
+				try
+				{
+					inchan = new BufferedReader(new InputStreamReader(client.getInputStream()));
+					outchan = new PrintWriter(new OutputStreamWriter(client.getOutputStream()));
+					while (true)
+					{
+						command = inchan.readLine();
+						System.out.println("Commande recu "+command);
+						if(command.equals("fin")) 
+						{ 
+							System.out.println("Fin de connexion.");
+							client.close();
+							serv.close();
+							break;
+						}
+						System.out.println("evoie de "+command.toUpperCase());
+						outchan.println(command.toUpperCase());
+						outchan.flush();
+					}
+				}
+				catch(IOException e) 
+				{ 
+					System.err.println("I/O Error"); e.printStackTrace();
+				}
+			}
+			
+		}
+		catch(Throwable t) {t.printStackTrace(System.err); }
+	}
+
+}
